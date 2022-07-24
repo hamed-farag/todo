dc2c9dca37c6206f529f4379e4e340b71e4cc799
Hamed Farag <hamed.farag.2009@gmail.com>
2022-07-24 20:19:07 +0200

    refactor: move calling todo api to Main page move TodoCard from business components to Home

--- src/Pages/Home/components/Listing/index.tsx
+++ src/Pages/Home/components/Listing/index.tsx
@@ -1,52 +1,21 @@
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-
 import Paginator from "@components/UI/Paginator";
-import TodoCard from "@components/Business/TodoCard";
-
-import env from "@configs/env";
-
-import todoService from "@services/todo";
+import TodoCard from "../TodoCard";
 
 import TodoInterface from "@interfaces/todo";
 
-import logger from "@helpers/logger";
-
 interface TodoListingProps {
-  selectedUser: string | null;
+  isLoading: boolean;
+  todoItems: Array<TodoInterface>;
+  totalCount: number;
+  pageSize: number;
+  getCurrentPage: (value: number) => void;
 }
 
 function TodoListing(props: TodoListingProps) {
-  const { selectedUser } = props;
-
-  const [todo, setTodo] = useState<Array<TodoInterface>>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [totalCount, setTotalCount] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const getTodoData = async (userId: string, pageNumber: string) => {
-    const [response, error] = await todoService.getTodoByUserId(userId, pageNumber, env.pageSize.toString());
-
-    if (response) {
-      setTodo(response.data);
-      setTotalCount(Number(response.headers["x-total-count"]));
-    } else {
-      toast.error("Cannot Fetch Todo for the selected User!");
-      logger.error("Something went Wrong, Cannot Fetch Todo!", error);
-    }
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    if (selectedUser) {
-      setIsLoading(true);
-      getTodoData(selectedUser, currentPage.toString());
-    }
-  }, [selectedUser, currentPage]);
+  const { isLoading, todoItems, totalCount, pageSize, getCurrentPage } = props;
 
   const renderTodo = () => {
-    const todoExtraction = todo.map((item: TodoInterface) => {
+    const todoExtraction = todoItems.map((item: TodoInterface) => {
       return <TodoCard key={item.id} data={item} onUpdate={() => {}} onDelete={() => {}} />;
     });
 
@@ -59,9 +28,9 @@ function TodoListing(props: TodoListingProps) {
       <Paginator
         totalCount={totalCount}
         disabled={false}
-        pageSize={env.pageSize}
+        pageSize={pageSize}
         onChange={(selectedPage) => {
-          setCurrentPage(selectedPage);
+          getCurrentPage(selectedPage);
         }}
       />
     </div>
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,15 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 import UsersDropdown from "@components/Business/Users";
 import TodoListing from "./components/Listing";
 
+import todoService from "@services/todo";
+
+import TodoInterface from "@interfaces/todo";
+
+import logger from "@helpers/logger";
+
+import env from "@configs/env";
+
 function Home() {
   const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+  const [todo, setTodo] = useState<Array<TodoInterface>>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getTodoData = async (userId: string, pageNumber: string) => {
+    const [response, error] = await todoService.getTodoByUserId(userId, pageNumber, env.pageSize.toString());
+
+    if (response) {
+      setTodo(response.data);
+      setTotalCount(Number(response.headers["x-total-count"]));
+    } else {
+      toast.error("Cannot Fetch Todo for the selected User!");
+      logger.error("Something went Wrong, Cannot Fetch Todo!", error);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (selectedUserId) {
+      setIsLoading(true);
+      getTodoData(selectedUserId, currentPage.toString());
+    }
+  }, [selectedUserId, currentPage]);
+
   return (
     <div>
       <UsersDropdown onChange={(userId) => setSelectedUserId(userId)} />
       <br />
-      <TodoListing selectedUser={selectedUserId} />
+      <TodoListing
+        isLoading={isLoading}
+        todoItems={todo}
+        totalCount={totalCount}
+        pageSize={env.pageSize}
+        getCurrentPage={(value) => {
+          setCurrentPage(value);
+        }}
+      />
     </div>
   );
 }
