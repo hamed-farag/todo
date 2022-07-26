cdd1784dc6aa2894eaa897fbb7dd1e4b70afcc40
Hamed Farag <hamed.farag.2009@gmail.com>
2022-07-26 21:41:19 +0200

    fix: set default language to english

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -16,7 +16,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "ar",
+    lng: "en",
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
